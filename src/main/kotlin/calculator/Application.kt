7d9374ddb6dbb7502319f8df0701b786d38bcbee
apdlwjwjwj <apdlwjwjwj@gmail.com>
2025-10-16 20:40:33 +0900

    feat(calculator): add custom separator
    Allow users to define custom separators

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -3,6 +3,12 @@ package calculator
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
+    println("덧셈할 문자 열을 입력해 주세요.")
     val input = Console.readLine()
-    println(input)
+
+    var separator = ","
+    if(input.startsWith("//")) {
+        separator = input.substring(2,input.indexOf("\\n"))
+    }
+    println("결과 : $separator")
 }
