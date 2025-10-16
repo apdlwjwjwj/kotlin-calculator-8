f6ce12364a61bc42d0c79417f106df39cc803b07
apdlwjwjwj <apdlwjwjwj@gmail.com>
2025-10-16 21:08:17 +0900

    feat(calculator): separate number

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,10 +5,17 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("덧셈할 문자 열을 입력해 주세요.")
     val input = Console.readLine()
-
+    val separators = mutableListOf(",",":")
+    var numberpart = input
     var separator = ","
+
     if(input.startsWith("//")) {
         separator = input.substring(2,input.indexOf("\\n"))
+        separators.add(separator)
+        numberpart = input.substring(input.indexOf("\\n")+2)
     }
-    println("결과 : $separator")
+
+    val numbers = numberpart.split(*separators.toTypedArray())
+        .map {it.toInt()}
+    println("결과 $numbers")
 }
