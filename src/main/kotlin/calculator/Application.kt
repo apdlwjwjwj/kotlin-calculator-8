468bcc04a6121d93f4137e56c6264c4eb49e9a84
apdlwjwjwj <apdlwjwjwj@gmail.com>
2025-10-17 17:49:25 +0900

    fix: update error handling logic
    Throw IllegalArgumentException when the sum of numbers is less than or equal to 0

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -6,21 +6,25 @@ fun main() {
     println("덧셈할 문자 열을 입력해 주세요.")
     val input = Console.readLine()
     val separators = mutableListOf(",",":")
-    var numberpart = input
+    var numberPart = input
     var separator = ","
 
     try {
         if (input.startsWith("//")) {
             separator = input.substring(2, input.indexOf("\\n"))
             separators.add(separator)
-            numberpart = input.substring(input.indexOf("\\n") + 1)
+            numberPart = input.substring(input.indexOf("\\n") + 2)
         }
 
-        val numbers = numberpart.split(*separators.toTypedArray())
-            .map { it.toInt() }
+        val numbers = numberPart.split(*separators.toTypedArray())
+            .map {
+                val num = it.toInt()
+                if (num < 0) throw IllegalArgumentException()
+                num
+            }
         val result = numbers.sum()
 
-        println("결과 $result")
+        println("결과 : $result")
     } catch (e: Exception){
         throw IllegalArgumentException()
     }
